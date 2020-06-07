10024d1716ab2dc53ee5ce3759f64f172f50aea0
obcat <obcat@icloud.com>
2020-06-08 03:53:23 +0900

    おい牛のパッケージ部分とjavascriptを追加

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 'use strict';
 
 {
-  const foo = document.getElementById('foo');
-
-  function updateValue(e) {
-    const canvas = document.querySelector('canvas');
-    if (typeof canvas.getContext === 'undefined') {
-      return;
-    }
-    const ctx = canvas.getContext('2d');
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height); //canvasのクリア
-
-    ctx.font = 'bold 32px Verdana';
-    ctx.fillText(e.target.value, 0, canvas.height / 2);
+  const inputs = document.getElementsByClassName('input');
+  const outputs = document.getElementsByClassName('output');
+  
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('input', e => {
+      
+      // e.targetでinput要素を取得
+      outputs[i].textContent = e.target.value;
+    })
   }
   
-  foo.addEventListener('input', updateValue);
+  function getImage() {
+    html2canvas(document.getElementById('pkg')).then(canvas => {
+      document.body.appendChild(canvas);
+    });
+  }
+
+  document.getElementById('generator').addEventListener('click', getImage);
 }
